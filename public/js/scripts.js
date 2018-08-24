563669525d16c5f5ac5415f0a6e7605d5664101f
Cragady <pinkdotdreamer@gmail.com>
2018-08-24 17:20:18 -0600

    added comment button; no functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,7 +4,7 @@ function dbCall(){
     }).then(returnUrl =>{
         $("#dump").empty();
         $.each(returnUrl, function(i, newb){
-            $("#dump").append(`<section class="card dump-scraper my-2 text-center p-1">
+            $("#dump").append(`<section class="card row dump-scraper my-2 text-center p-1">
                 <div class="headline-scrape card-header">
                     <h3>${returnUrl[i].headline}</h3>
                 </div>
@@ -13,7 +13,12 @@ function dbCall(){
                 </div>
                 <div class="url-scrape">
                     <a href="${returnUrl[i].url}">${returnUrl[i].url}</a>
-                </div
+                </div>
+                <div class="row m-2 card-footer">
+                    <button class="btn" data-id="${returnUrl[i]._id}">Leave a Comment!</button>
+                    <div class="comment-dump col-12 border-top mt-3" id="${returnUrl[i]._id}" >test
+                    test</div>
+                </div>
             </section>`);
         });
     });
